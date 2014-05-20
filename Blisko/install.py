1c98570fe811fd10b9a45aca7b91cfd3fd7f2c41
jkonieczny <jkonieczny91@o2.pl>
2014-05-20 10:09:10 +0200

    merge, wczytywanie kursu

--- Blisko/install.py
+++ Blisko/install.py
@@ -5,6 +5,7 @@ __author__ = 'jkonieczny'
 """
 
 from web.models import *
+import json
 
 def install_basic():
     """
@@ -20,4 +21,48 @@ def install_tests():
     """
     Instalacja kursów
     """
+    kursy = ["JSON"]
+    for kurs in kursy:
+        dir = BASE_DIR+"/kursy/"+kurs+"/"
+        with open(dir+"data.json") as data:
+            course = Course()
+            data = json.loads(data.read())
+            course.name = data['title']
+            course.short = data['short']
+            course.long = data['long']
+            course.save()
+            #objs = []
+            for lesson in data['lessons']:
+                les = Lesson()
+                les.name = data['title']
+                with open(dir+"/"+lesson['file']) as content:
+                    les.content = content.read()
+                les.course = course
+                #objs.append(les)
+                les.save()
+            for test in data['tests']:
+                t = Test()
+                t.points = len(len(ts['questions']))
+                with open(dir+"/"+test['file']) as ts:
+                    ts = json.loads(ts)
+                    t.name = ts['title']
+                    for question in ts['questions']:
+                        quest = Question()
+                        quest.type = Question.CLOSED_BINARY
+                        quest.test = t
+                        question.content =  question['question']
+                        for ans in question['correct']:
+                            a = Answer()
+                            a.question = quest
+                            a.correct = True
+                            a.text = ans
+                            a.save()
+                        for ans in question['false']:
+                            a = Answer()
+                            a.question = quest
+                            a.correct = False
+                            a.text = ans
+                            a.save()
+                    t.course = course
+                    t.save()
     pass
