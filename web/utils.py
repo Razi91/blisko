9c08ed83cb15edf7b4a61a63d1f108d2d880636c
jkonieczny <jkonieczny91@o2.pl>
2014-05-25 23:41:51 +0200

    fix

--- web/utils.py
+++ web/utils.py
@@ -6,4 +6,4 @@ def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
 def check_password(hashed_password, user_password):
-    return hashed_password == hashlib.sha256(user_password.encode()).hexdigest()
+    return hashed_password == hash_password(user_password)
