d8dd7edcc0ac4aab3436af1459597f992eb3d04f
jkonieczny <jkonieczny91@o2.pl>
2014-05-25 12:59:30 +0200

    model

--- web/models.py
+++ web/models.py
@@ -20,6 +20,7 @@ class User(models.Model):
     login = models.CharField(max_length=30)
     password = models.CharField(max_length=128)
     #real_name = models.CharField(max_length=30)
+    #email = models.EmailField()
     privilages = models.ForeignKey(AccountPrivilages)
     enabled = models.BooleanField(default=0)
     credits = models.IntegerField(default=0)
